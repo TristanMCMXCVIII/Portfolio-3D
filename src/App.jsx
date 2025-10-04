3de7ce21b9d43fd4c0f3372707fde194dde1806e
Tristan <nocent.tristan@gmail.com>
2025-10-04 12:14:51 +0200

    logo showcase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
+import LogoShowcase from "./sections/LogoShowcase.jsx";
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <ShowcaseSection />
+            <LogoShowcase />
         </>
     )
 }
