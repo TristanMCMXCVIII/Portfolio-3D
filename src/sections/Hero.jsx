53b1da6315e01f6300f2c6e224d48ef8178adaf3
Tristan <nocent.tristan@gmail.com>
2025-10-04 11:08:36 +0200

    Hero animation GSAP

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,28 @@ import { words } from "../constants";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
 
+import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
+
 
 const Hero = () => {
+
+    useGSAP(()=> {
+        gsap.fromTo('.hero-text h1',
+            {
+                y:50,
+                opacity:0
+            },
+            {
+                y:0,
+                opacity:1,
+                stagger: 0.2,
+                duration: 1,
+                ease: 'power2.inOut'
+            },
+            )
+    });
 
     return (
         <section id="hero" className="relative overflow-hidden">
@@ -61,7 +81,7 @@ const Hero = () => {
                 </figure>
             </div>
 
-
+            <AnimatedCounter />
         </section>
     );
 };
