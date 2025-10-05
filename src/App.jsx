c734d6bcea52b143a8f647f754079022d5439af0
Tristan <nocent.tristan@gmail.com>
2025-10-05 10:31:13 +0200

    feature cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./sections/Hero.jsx";
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import LogoShowcase from "./sections/LogoShowcase.jsx";
+import FeatureCards from "./sections/FeatureCards.jsx";
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
             <Hero />
             <ShowcaseSection />
             <LogoShowcase />
+            <FeatureCards />
         </>
     )
 }
