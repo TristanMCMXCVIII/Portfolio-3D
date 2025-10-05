3147f6f2a58d2eb759415b93efd95c5bdb73f5c5
Tristan <nocent.tristan@gmail.com>
2025-10-05 11:24:40 +0200

    work experience with GSAP

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import NavBar from "./components/NavBar.jsx";
 import LogoShowcase from "./sections/LogoShowcase.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
+import ExperienceSection from "./sections/ExperienceSection.jsx";
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
             <ShowcaseSection />
             <LogoShowcase />
             <FeatureCards />
+            <ExperienceSection />
         </>
     )
 }
